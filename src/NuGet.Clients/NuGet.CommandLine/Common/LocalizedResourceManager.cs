d311303bffec18d3c65d2e8f569e35a642d913e4
Joel Verhagen <jver@microsoft.com>
2016-04-04 17:27:11 -0700

    Undo unnecessary changes

--- src/NuGet.Clients/NuGet.CommandLine/Common/LocalizedResourceManager.cs
+++ src/NuGet.Clients/NuGet.CommandLine/Common/LocalizedResourceManager.cs
@@ -6,15 +6,13 @@ namespace NuGet
 {
     internal static class LocalizedResourceManager
     {
-        public static CultureInfo Culture { get; set; }
-        public static ResourceManager ResourceManager { get; private set; } =
-            new ResourceManager("NuGet.CommandLine.NuGetResources", typeof(LocalizedResourceManager).Assembly);
+        private static readonly ResourceManager _resourceManager = new ResourceManager("NuGet.CommandLine.NuGetResources", typeof(LocalizedResourceManager).Assembly);
 
         public static string GetString(string resourceName)
         {
             var culture = GetLanguageName();
-            return ResourceManager.GetString(resourceName + '_' + culture, CultureInfo.InvariantCulture) ??
-                   ResourceManager.GetString(resourceName, CultureInfo.InvariantCulture);
+            return _resourceManager.GetString(resourceName + '_' + culture, CultureInfo.InvariantCulture) ??
+                   _resourceManager.GetString(resourceName, CultureInfo.InvariantCulture);
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1308:NormalizeStringsToUppercase", Justification = "the convention is to used lower case letter for language name.")]        
@@ -24,19 +22,15 @@ namespace NuGet
         /// <returns>the 3 letter language name used to locate localized resources.</returns>
         public static string GetLanguageName()
         {
-            CultureInfo culture = Culture;
-            if (culture == null)
+            var culture = Thread.CurrentThread.CurrentUICulture;
+            while (!culture.IsNeutralCulture)
             {
-                culture = Thread.CurrentThread.CurrentUICulture;
-                while (!culture.IsNeutralCulture)
+                if (culture.Parent == culture)
                 {
-                    if (culture.Parent == culture)
-                    {
-                        break;
-                    }
-
-                    culture = culture.Parent;
+                    break;
                 }
+
+                culture = culture.Parent;
             }
 
             return culture.ThreeLetterWindowsLanguageName.ToLowerInvariant();
